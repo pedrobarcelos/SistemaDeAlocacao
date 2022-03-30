64e2950e71ab22a92bc3bc039454133eec876289
Pedro Barcelos <pbarcelos56@icloud.com>
2022-03-30 01:50:27 -0300

    Implementação do CREATE

--- src/main/java/grupo5/SistemaDeAlocacao/rh/controle/ClienteControle.java
+++ src/main/java/grupo5/SistemaDeAlocacao/rh/controle/ClienteControle.java
@@ -1,9 +1,12 @@
 package grupo5.SistemaDeAlocacao.rh.controle;
 
+import grupo5.SistemaDeAlocacao.rh.dominio.Cliente;
 import grupo5.SistemaDeAlocacao.rh.dominio.ClienteRepositorio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ClienteControle {
@@ -18,6 +21,17 @@ public class ClienteControle {
     public String clientes(Model model){
         model.addAttribute("listaClientes", clienteRepo.findAll());
         return "rh/clientes/index";
+    }
+
+    @GetMapping("/rh/clientes/novo")
+    public String novoCliente(@ModelAttribute("cliente") Cliente cliente){
+        return "rh/clientes/form";
+    }
+
+    @PostMapping("/rh/clientes/salvar")
+    public String salvarPessoa(@ModelAttribute("cliente") Cliente cliente){
+        clienteRepo.save(cliente);
+        return "redirect:/rh/clientes";
     }
 
 }
